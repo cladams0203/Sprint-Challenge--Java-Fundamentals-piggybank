25a1830fd5301e86b30ba443a5fa036cde5ef765
cladams0203 <cladams0203@gmail.com>
2020-08-27 16:35:40 -0400

    MVP reached

--- src/main/java/com/lambdaschool/piggybank/controllers/CoinController.java
+++ src/main/java/com/lambdaschool/piggybank/controllers/CoinController.java
@@ -1,9 +1,16 @@
 package com.lambdaschool.piggybank.controllers;
 
 
+import com.lambdaschool.piggybank.models.Coin;
 import com.lambdaschool.piggybank.repositories.CoinRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @RestController
 public class CoinController {
@@ -11,4 +18,22 @@ public class CoinController {
     CoinRepository coinRepository;
 
     //http://localhost:2019/total
+    @GetMapping(value = "/total", produces = {"application/json"})
+    public ResponseEntity<?> ListallCoins(){
+        List<Coin> coinList = new ArrayList<>();
+        coinRepository.findAll().iterator().forEachRemaining(coinList::add);
+        double total = 0;
+        for (Coin c : coinList){
+            if(c.getQuantity() > 1 ){
+                System.out.println(c.getQuantity() + "  " + c.getNameplural());
+                total = total + c.getQuantity() * c.getValue();
+
+            }else {
+                System.out.println(c.getQuantity() + "  " + c.getName());
+                total = total + c.getValue();
+            }
+        }
+        System.out.println("The Piggy Bank Holds " + total);
+        return new ResponseEntity<>(coinList, HttpStatus.OK);
+    }
 }
